feab75dcab72614fdc1d16da9163d6c17c55087b
karta1902613 <105021043@live.asia.edu.tw>
2016-10-24 16:58:05 +0800

    1024 ex 5
    1024 ex 5

--- src/ex/ex05.java
+++ src/ex/ex05.java
@@ -2,14 +2,22 @@
 /*
  * Topic: 設計一個程式由 0~9 的亂數隨機產生一 10*10 的二維陣列，將行列互換輸出(即輸出轉置矩陣)
  * Date: 2016/10/24
- * Author: 1050210XX 周永振老師
+ * Author: 105021043 劉凱文
  */
-
+import java.util.Random;
 public class ex05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
-}
+		Random ran=new Random();
+	    		int [][]data =new int[10][10];
+	    		for(int i=0;i<10;i++){
+	    			for(int j=0;j<10;j++){
+	    				data[i][j]=ran.nextInt(10);
+	    				System.out.print(data[i][j]+" ");}
+	    			System.out.println();}
+	    		System.out.println("------轉--換--後------");
+	  	    		for(int i=0;i<10;i++){
+	    			for(int j=0;j<10;j++){
+	 	    				System.out.print(data[j][i]+" ");}
+	    			System.out.println();}}}

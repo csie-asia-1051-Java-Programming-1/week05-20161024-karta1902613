e25352227c8dd76fd88383dbfdf1189ee1148d0c
karta1902613 <105021043@live.asia.edu.tw>
2016-10-24 21:43:18 +0800

    1024 HW34
    1024 HW34

--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -1,10 +1,19 @@
 package hw;
 
+import java.util.Scanner;
+
+/*
+ * Topic: 設計一個程式，讓使用者輸入一個正整數n，判斷並輸出n的多少次方才會大於10000
+ * Date: 2016/10/24
+ * Author: 105021043 劉凱文
+ */
 public class hw03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		System.out.print(Math.ceil((Math.log(10000))/Math.log(n)));
 	}
 
 }
--- src/hw/hw04.java
+++ src/hw/hw04.java
@@ -1,10 +1,19 @@
 package hw;
 
-public class hw04 {
+import java.util.Scanner;
 
+public class hw04 {
+	/*
+	 * Topic: 讓使用者輸入一正整數n，計算1/(1*2)+1/(3*4)+1/(5*6)+…+1/[(2n-1)*(2n)]並輸出結果
+	 * Date: 2016/10/24
+	 * Author: 105021043 劉凱文
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
-}
+		Scanner scn=new Scanner(System.in);
+		float sum=0;
+		float n=scn.nextInt();
+		for(float i=1;i<=n;i++){
+			float x=(float)1/((2*i-1)*(2*i));
+			 sum+=x;}
+		System.out.print(sum);}}

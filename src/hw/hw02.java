e7d149ea665b244370ecc1416d01f984f28fe938
karta1902613 <105021043@live.asia.edu.tw>
2016-10-24 18:27:49 +0800

    1024 hw02
    1024 hw02

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,30 @@
 package hw;
-
+/*
+ * Topic: 讓使用者輸入一正整數n，輸出1+2+3+…+m<=n的最大整數m=?
+ * Date: 2016/10/24
+ * Author: 105021043 劉凱文
+ */
+import java.util.Scanner;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int m=0;
 
+		int sum=0;
+				while(sum<n){
+			m++;
+			sum=sum+m;
+			if(sum>n){
+				System.out.print(m-1);}
+				else if(sum>=n){
+					System.out.print(m);
+				}
+			}
+						
+				
+		}
 	}
 
-}
